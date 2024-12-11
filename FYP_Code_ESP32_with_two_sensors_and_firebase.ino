85c56b0ebc236f5c55308ea5d271ab65401753bc
Mahmood-Ghauri <ghauribrother12@gmail.com>
2024-12-11 17:46:22 +0500

    Add files via upload
    Updated code of esp32 with two sensors and firebase & sd card interfaced 
    the problem was memory reaches its limit

--- FYP_Code_ESP32_with_two_sensors_and_firebase.ino
+++ FYP_Code_ESP32_with_two_sensors_and_firebase.ino
@@ -17,89 +17,78 @@ int cs = 27;
 RTC_DS3231 rtc; // Create an instance of the RTC_DS3231 class
 
 // Variables to store received data of iron
-String I_cycle = "0";
-float I_voltage = 0.0;
-float I_current = 0.0;
-float I_app_power = 0.0;
-float I_react_power = 0.0;
-float I_act_power = 0.0;
+String I_cycle = "1";
+float I_voltage = 1;
+float I_current = 1;
+float I_app_power = 1;
+float I_react_power = 1;
+float I_act_power = 1;
 
 // Variables to store received data of vacuum cleaner
-String V_cycle = "0";
-float V_voltage = 0.0;
-float V_current = 0.0;
-float V_app_power = 0.0;
-float V_react_power = 0.0;
-float V_act_power = 0.0;
+String V_cycle = "1";
+float V_voltage = 1;
+float V_current = 1;
+float V_app_power = 1;
+float V_react_power = 1;
+float V_act_power = 1;
 
 // Variables to store received data of fridge
-String F_cycle = "0";
-float F_voltage = 0.0;
-float F_current = 0.0;
-float F_app_power = 0.0;
-float F_react_power = 0.0;
-float F_act_power = 0.0;
+String F_cycle = "1";
+float F_voltage = 1;
+float F_current = 1;
+float F_app_power = 1;
+float F_react_power = 1;
+float F_act_power = 1;
 
 // Variables to store received data of Oven
-bool O_cycle = 0;
-float O_voltage = 0.0;
-float O_current = 0.0;
-float O_app_power = 0.0;
-float O_react_power = 0.0;
-float O_act_power = 0.0;
-float O_power_fact = 0.0;
+bool O_cycle = 1;
+float O_voltage = 1;
+float O_current = 1;
+float O_app_power = 1;
+float O_react_power = 1;
+float O_act_power = 1;
+float O_power_fact = 1;
 
 // Variables to store received data of Overall Load
-bool L_cycle = 0;
-float L_voltage = 0.0;
-float L_current = 0.0;
-float L_app_power = 0.0;
-float L_react_power = 0.0;
-float L_act_power = 0.0;
-float L_power_fact = 0.0;
+bool L_cycle = 1;
+float L_voltage = 1;
+float L_current = 1;
+float L_app_power = 1;
+float L_react_power = 1;
+float L_act_power = 1;
+float L_power_fact = 1;
 
 String timedate = "NULL";
-float voltage = 0.0;
 float total_power = 0.0;
 
+String receivedData;
+String receivedData_1;
+String receivedData_2;
+
 //Firebase Setup
-#include <Arduino.h>
 #include <WiFi.h>
-#include <FirebaseESP32.h>
+#include <Firebase_ESP_Client.h>
 
-// Provide the token generation process info.
-#include <addons/TokenHelper.h>
-
-// Provide the RTDB payload printing info and other helper functions.
-#include <addons/RTDBHelper.h>
-
-/* 1. Define the WiFi credentials */
+// Your WiFi credentials
 #define WIFI_SSID "MAUG"
 #define WIFI_PASSWORD "d24e95dc"
 
-// For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
-
-/* 2. Define the API Key */
+// Firebase project credentials
 #define API_KEY "AIzaSyDBcxIo-G9o1Q_sEJx2Ly1NYT52YibvUFk"
+#define DATABASE_URL "https://fyp-aiot-smart-meter-dataset-default-rtdb.asia-southeast1.firebasedatabase.app/" // Database URL
 
-/* 3. Define the RTDB URL */
-#define DATABASE_URL "https://fyp-aiot-smart-meter-dataset-default-rtdb.asia-southeast1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
-
-/* 4. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL "ghauribrother12@gmail.com"
-#define USER_PASSWORD "MAhm12##"
-
-// Define Firebase Data object
 FirebaseData fbdo;
-
-FirebaseAuth auth;
 FirebaseConfig config;
+FirebaseAuth auth;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;  // Send data every 5 second
 
 void setup() {
   // Initialize Serial ports
   Serial.begin(9600);                             // Oven (Appliance 04)
-  Serial1.begin(9600, SERIAL_8N1, 16, 17);        // Total (Overall Power Consumption)
-  Serial2.begin(9600, SERIAL_8N1, 4, 2);          // For serial Communication with other ESP32 for other Appliances Values
+  Serial1.begin(9600, SERIAL_8N1, 4, 2 );      //RX,TX  // Total (Overall Power Consumption)
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);          // For serial Communication with other ESP32 for other Appliances Values
   Serial.println("Waiting for data...");
 
   // Initialize RTC Date & Time
@@ -110,7 +99,8 @@ void setup() {
   }
   // Uncomment this if you want to reset the time and date
   // Set RTC to the current Unix time
-  //rtc.adjust(DateTime(UnixTimeToDate(1700000000)));
+  // unsigned long unixTime = 1733249000;  // Example Unix timestamp
+  // rtc.adjust(DateTime(unixTime));
 
   // Initialize SD Card
   #ifdef REASSIGN_PINS
@@ -132,63 +122,46 @@ void setup() {
     delay(100);
   }
 
-  // Connect to Wi-Fi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to Wi-Fi...");
   }
+  Serial.println("Connected to Wi-Fi");
 
-  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
-
-  /* Assign the api key (required) */
+  // Firebase configuration
   config.api_key = API_KEY;
-
-  /* Assign the user sign in credentials */
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
-  /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  // Assign the user authentication (optional)
+  auth.user.email = "ghauribrother12@gmail.com";       // Optional for authentication
+  auth.user.password = "MAhm12##"; // Optional for authentication
 
-  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
-  Firebase.reconnectNetwork(true);
-
-  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
-
+  // Initialize Firebase
   Firebase.begin(&config, &auth);
-
-  Firebase.setDoubleDigits(5);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
 
   // Check if data is available on Serial (IRON)
-  if (Serial.available() > 0) {
-    String receivedData = Serial.readStringUntil('\n'); // Read the data line
-
-    // Calling Function to calculate values of iron
+  while (Serial.available() > 0) {
+    Serial.print("Oven");
+    receivedData = Serial.readStringUntil('\n');
     ovenData(receivedData);
   }
 
   // Check if data is available on Serial1 (VACUUM CLEANER)
-  if (Serial1.available() > 0) {
-    String receivedData_1 = Serial1.readStringUntil('\n'); // Read the data line
-
-    // Calling Function to calculate values of vacuum cleaner
+  while (Serial2.available() > 0) {
+    Serial.print("Overall Load");
+    receivedData_1 = Serial2.readStringUntil('\n');
     loadData(receivedData_1);
   }
 
   // Check if data is available on Serial2 (FRIDGE)
-  if (Serial2.available() > 0) {
-    String receivedData_2 = Serial2.readStringUntil('\n'); // Read the data line
-
-    // Calling Function to calculate values of fridge
+  while (Serial1.available() > 0) {
+    Serial.print("Fridge, Vacuum, Iron");
+    receivedData_2 = Serial1.readStringUntil('\n');
     otherData(receivedData_2);
   }
 
@@ -208,8 +181,7 @@ void loop() {
   file.print(timedate);
   file.print(",");
 
-  voltage = (I_voltage+V_voltage+F_voltage+O_voltage)/4;
-  file.print(voltage);
+  file.print(L_voltage);
   file.print(",");
 
   file.print(I_current);
@@ -265,45 +237,58 @@ void loop() {
 
   overallData();
 
-  FirebaseJson json;
+  // Send data every `timerDelay` milliseconds
+  if ((millis() - lastTime) > timerDelay) {
+    lastTime = millis();
 
-  // Populate JSON object with the data
-  json.add("TimeDate",timedate);
+    // JSON object to store data
+    String path = "/sensorData/" + String(millis()); // Timestamp as key
+    FirebaseJson json;
+    // Populate JSON object with the data
+    json.set("/TimeDate",timedate);
 
-  json.add("Voltage",voltage);
+    json.set("/Voltage",L_voltage);
 
-  json.add("Iron_Current",I_current);
-  json.add("Vacuum_Current",V_current);
-  json.add("Fridge_Current",F_current);
-  json.add("Oven_Current",O_current);
+    json.set("/Iron_Current",I_current);
+    json.set("/Vacuum_Current",V_current);
+    json.set("/Fridge_Current",F_current);
+    json.set("/Oven_Current",O_current);
 
-  json.add("Total_Power",total_power);
+    json.set("/Total_Power",total_power);
 
-  json.add("Iron_Act_Power",I_act_power);
-  json.add("Vacuum_Act_Power",V_act_power);
-  json.add("Fridge_Act_Power",F_act_power);
-  json.add("Oven_Act_Power",O_act_power);
+    json.set("/Iron_Act_Power",I_act_power);
+    json.set("/Vacuum_Act_Power",V_act_power);
+    json.set("/Fridge_Act_Power",F_act_power);
+    json.set("/Oven_Act_Power",O_act_power);
 
-  json.add("Iron_ReAct_Power",I_react_power);
-  json.add("Vacuum_ReAct_Power",V_react_power);
-  json.add("Fridge_ReAct_Power",F_react_power);
-  json.add("Oven_ReAct_Power",O_react_power);
+    json.set("/Iron_ReAct_Power",I_react_power);
+    json.set("/Vacuum_ReAct_Power",V_react_power);
+    json.set("/Fridge_ReAct_Power",F_react_power);
+    json.set("/Oven_ReAct_Power",O_react_power);
 
-  json.add("Iron_App_Power",I_app_power);
-  json.add("Vacuum_App_Power",V_app_power);
-  json.add("Fridge_App_Power",F_app_power);
-  json.add("Oven_App_Power",O_app_power);
+    json.set("/Iron_App_Power",I_app_power);
+    json.set("/Vacuum_App_Power",V_app_power);
+    json.set("/Fridge_App_Power",F_app_power);
+    json.set("/Oven_App_Power",O_app_power);
 
-  json.add("Iron_Cycle",I_cycle);
-  json.add("Vacuum_Cycle",V_cycle);
-  json.add("Fridge_Cycle",F_cycle);
-  json.add("Oven_Cycle",O_cycle);
+    json.set("/Iron_Cycle",I_cycle);
+    json.set("/Vacuum_Cycle",V_cycle);
+    json.set("/Fridge_Cycle",F_cycle);
+    json.set("/Oven_Cycle",O_cycle);
 
+    // Send data to Firebase
+    if (Firebase.RTDB.setJSON(&fbdo, path.c_str(), &json)) {
+      Serial.println("Data sent successfully:");
+    } else {
+      Serial.print("Failed to send data: ");
+      Serial.println(fbdo.errorReason());
+    }
+  }
   delay(2000); // Delay to avoid flooding with data
 }
 
 void overallData() {
-  File file = SD.open("alldata.txt", FILE_APPEND);
+  File file = SD.open("/alldata.txt", FILE_APPEND);
   while (!file) 
   {
     Serial.println("Failed to open file for writing");
@@ -343,29 +328,34 @@ void ovenData(String data) {
   int oapp_powerIndex = data.indexOf("Apparent Power:");
   int opower_factIndex = data.indexOf("Power Factor:");
  
-  if (ovoltageIndex != -1 && ocurrentIndex != -1 && oapp_powerIndex != -1 && opower_factIndex != -1) {
-    // Extract the voltage value
+  if (ovoltageIndex != -1)
+  {
     O_voltage = data.substring(ovoltageIndex + 8, data.indexOf("V", ovoltageIndex)).toFloat();
+  }
 
-    // Extract the current value
+  if (ocurrentIndex != -1)
+  {
     O_current = data.substring(ocurrentIndex + 8, data.indexOf("A", ocurrentIndex)).toFloat();
+  }
 
-    // Extract the apparent power value
-    O_app_power = data.substring(oapp_powerIndex + 15, data.indexOf("VA", oapp_powerIndex)).toFloat();
-
-    // Extract the power factor value
+  if (opower_factIndex != -1)
+  {
     O_power_fact = data.substring(opower_factIndex + 13, data.indexOf("\n", opower_factIndex)).toFloat();
+  }
 
-    // Calculation of State of Appliance
-    if (O_current >= 0.5){
-      O_cycle = 1;
-    }
-
-    // Calculation of active power
+  if (oapp_powerIndex != -1)
+  {
+    O_app_power = data.substring(oapp_powerIndex + 15, data.indexOf("VA", oapp_powerIndex)).toFloat();
     O_act_power = O_app_power * O_power_fact;
-
-    // Calculation of reactive power
     O_react_power = sqrt(pow(O_app_power,2) - pow(O_act_power,2));
+  }
+  if (O_current >= 0.5)
+  {
+    O_cycle = 1;
+  }
+  if (O_current <= 0.5)
+  {
+    O_cycle = 0;
   }
 }
 
@@ -376,29 +366,34 @@ void loadData(String data) {
   int lapp_powerIndex = data.indexOf("Apparent Power:");
   int lpower_factIndex = data.indexOf("Power Factor:");
  
-  if (lvoltageIndex != -1 && lcurrentIndex != -1 && lapp_powerIndex != -1 && lpower_factIndex != -1) {
-    // Extract the voltage value
+  if (lvoltageIndex != -1)
+  {
     L_voltage = data.substring(lvoltageIndex + 8, data.indexOf("V", lvoltageIndex)).toFloat();
+  }
 
-    // Extract the current value
+  if (lcurrentIndex != -1)
+  {
     L_current = data.substring(lcurrentIndex + 8, data.indexOf("A", lcurrentIndex)).toFloat();
+  }
 
-    // Extract the apparent power value
-    L_app_power = data.substring(lapp_powerIndex + 15, data.indexOf("VA", lapp_powerIndex)).toFloat();
-
-    // Extract the power factor value
+  if (lpower_factIndex != -1)
+  {
     L_power_fact = data.substring(lpower_factIndex + 13, data.indexOf("\n", lpower_factIndex)).toFloat();
+  }
 
-    // Calculation of State of Appliance
-    if (L_current >= 0.5){
-      L_cycle = 1;
-    }
-
-    // Calculation of active power
+  if (lapp_powerIndex != -1)
+  {
+    L_app_power = data.substring(lapp_powerIndex + 15, data.indexOf("VA", lapp_powerIndex)).toFloat();
     L_act_power = L_app_power * L_power_fact;
-
-    // Calculation of reactive power
     L_react_power = sqrt(pow(L_app_power,2) - pow(L_act_power,2));
+  }
+  if (L_current >= 0.5)
+  {
+    L_cycle = 1;
+  }
+  if (L_current <= 0.5)
+  {
+    L_cycle = 0;
   }
 }
 
@@ -411,24 +406,22 @@ void otherData(String data) {
   int iapp_powerIndex = data.indexOf("Iron Apparent Power: ");
   int icycleIndex = data.indexOf("Iron Cycle: ");
  
-  if (ivoltageIndex != -1 && icurrentIndex != -1 && iact_powerIndex != -1 && ireact_powerIndex != -1 && iapp_powerIndex != -1 && icycleIndex != -1) {
-
-    // Extract the voltage value
+  if (ivoltageIndex != -1) {
     I_voltage = data.substring(ivoltageIndex + 14, data.indexOf(" V", ivoltageIndex)).toFloat();
-    
-    // Extract the current value
+  }
+  if (icurrentIndex != -1) {
     I_current = data.substring(icurrentIndex + 14, data.indexOf(" A", icurrentIndex)).toFloat();
-    
-    // Extract the active power value
+  }
+  if (iact_powerIndex != -1) {
     I_act_power = data.substring(iact_powerIndex + 19, data.indexOf(" W", iact_powerIndex)).toFloat();
-
-    // Extract the reactive power value
+  }
+  if (ireact_powerIndex != -1) {
     I_react_power = data.substring(ireact_powerIndex + 21, data.indexOf(" VAR", ireact_powerIndex)).toFloat();
-
-    // Extract the apparent power value
+  }
+  if (iapp_powerIndex != -1) {
     I_app_power = data.substring(iapp_powerIndex + 21, data.indexOf(" VA", iapp_powerIndex)).toFloat();
-
-    // Extract the cycle value
+  }
+  if (icycleIndex != -1) {
     I_cycle = data.substring(icycleIndex + 12, data.indexOf("\n", icycleIndex));
   }
 
@@ -440,24 +433,22 @@ void otherData(String data) {
   int vapp_powerIndex = data.indexOf("Vacuum Apparent Power: ");
   int vcycleIndex = data.indexOf("Vacuum Cycle: ");
  
-  if (vvoltageIndex != -1 && vcurrentIndex != -1 && vact_powerIndex != -1 && vreact_powerIndex != -1 && vapp_powerIndex != -1 && vcycleIndex != -1) {
-
-    // Extract the voltage value
+  if (vvoltageIndex != -1) {
     V_voltage = data.substring(vvoltageIndex + 16, data.indexOf(" V", vvoltageIndex)).toFloat();
-    
-    // Extract the current value
+  }
+  if (vcurrentIndex != -1) {
     V_current = data.substring(vcurrentIndex + 16, data.indexOf(" A", vcurrentIndex)).toFloat();
-    
-    // Extract the active power value
+  }
+  if (vact_powerIndex != -1) {
     V_act_power = data.substring(vact_powerIndex + 21, data.indexOf(" W", vact_powerIndex)).toFloat();
-
-    // Extract the reactive power value
+  }
+  if (vreact_powerIndex != -1) {
     V_react_power = data.substring(vreact_powerIndex + 23, data.indexOf(" VAR", vreact_powerIndex)).toFloat();
-
-    // Extract the apparent power value
+  }
+  if (vapp_powerIndex != -1) {
     V_app_power = data.substring(vapp_powerIndex + 23, data.indexOf(" VA", vapp_powerIndex)).toFloat();
-
-    // Extract the cycle value
+  }
+  if (vcycleIndex != -1) {
     V_cycle = data.substring(vcycleIndex + 14, data.indexOf("\n", vcycleIndex));
   }
 
@@ -469,24 +460,22 @@ void otherData(String data) {
   int fapp_powerIndex = data.indexOf("Fridge Apparent Power: ");
   int fcycleIndex = data.indexOf("Fridge Cycle: ");
  
-  if (fvoltageIndex != -1 && fcurrentIndex != -1 && fact_powerIndex != -1 && freact_powerIndex != -1 && fapp_powerIndex != -1 && fcycleIndex != -1) {
-
-    // Extract the voltage value
+  if (fvoltageIndex != -1) {
     F_voltage = data.substring(fvoltageIndex + 16, data.indexOf(" V", fvoltageIndex)).toFloat();
-    
-    // Extract the current value
+  }
+  if (fcurrentIndex != -1) {
     F_current = data.substring(fcurrentIndex + 16, data.indexOf(" A", fcurrentIndex)).toFloat();
-    
-    // Extract the active power value
+  }
+  if (fact_powerIndex != -1) {
     F_act_power = data.substring(fact_powerIndex + 21, data.indexOf(" W", fact_powerIndex)).toFloat();
-
-    // Extract the reactive power value
+  }
+  if (freact_powerIndex != -1) {
     F_react_power = data.substring(freact_powerIndex + 23, data.indexOf(" VAR", freact_powerIndex)).toFloat();
-
-    // Extract the apparent power value
+  }
+  if (fapp_powerIndex != -1) {
     F_app_power = data.substring(fapp_powerIndex + 23, data.indexOf(" VA", fapp_powerIndex)).toFloat();
-
-    // Extract the cycle value
+  }
+  if (fcycleIndex != -1) {
     F_cycle = data.substring(fcycleIndex + 14, data.indexOf("\n", fcycleIndex));
   }
 }
